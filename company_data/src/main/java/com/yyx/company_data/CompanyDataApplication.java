bb783ed44a193388b998210e81d6d2d32a6eb50a
杨亚鑫 <501005280@qq.com>
2019-12-14 17:56:44 +0800

    Initial commit

---
+++ company_data/src/main/java/com/yyx/company_data/CompanyDataApplication.java
@@ -0,0 +1,13 @@
+package com.yyx.company_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyDataApplication.class, args);
+    }
+
+}
---
+++ company_data/src/test/java/com/yyx/company_data/CompanyDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyx.company_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
